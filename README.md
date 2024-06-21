1c780c4a9409e90ec03f1645a080d6f8013ace28
Takasur Azeem <takasurazeem@gmail.com>
2024-06-21 18:10:43 +0500

    Added navigator and mp4 for README demo

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# onLongPressGesture
+[![Recorded video of the onLongPressGesture example](RecordedVideos/onLongPressGesture.mp4)](RecordedVideos/onLongPressGesture.mp4)
--- SwiftfulThinkingIntermediate/ContentView.swift
+++ SwiftfulThinkingIntermediate/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                NavigationLink {
+                    LongPressGestureBootcamp()
+                } label: {
+                    Text("LongPressGestureBootcamp")
+                }
+            }
+            .navigationTitle("Swiftful - Intermediate")
         }
-        .padding()
     }
 }
 
